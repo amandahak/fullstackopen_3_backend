84b145b1cbe78eca9693f0f63c704fac4e2bc868
Nadina <nadinalehtonen@kamk.fi>
2024-09-11 19:15:24 +0300

    tehtävä 3.4

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ app.use(express.json())
 
 let persons = [
   {
-    "id": "1",
+    "id": 1,
     "name": "Arto Hellas",
     "number": "040-123456"    
   },
   {
-    "id": "2",
+    "id": 2,
     "name": "Ada Lovelace",
     "number": "39-44-5323523"    
   },
   {
-    "id": "3",
+    "id": 3,
     "name": "Dan Abramov",
     "number": "12-43-234345"    
   },
   {
-    "id": "4",
+    "id": 4,
     "name": "Mary Poppendieck",
     "number": "39-23-6423122" 
   }
@@ -30,26 +30,36 @@ app.get('/', (request, response) => {
   response.send('<h1>Puhelinluettelo</h1>')
 })
 
+app.get('/info', (request, response) => {
+    const count = persons.length
+    const text = `Puhelinluettelossa on tietoa ${count} kontaktista`
+    const date = new Date().toString()
+    response.send(
+      `<p>${text}<br><br>${date}</p>`
+    )
+  })
+  
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
-})
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+  
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).json({ error: 'Person not found' })
+    }
+  })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+const id = Number(request.params.id)
+persons = persons.filter(person => person.id !== id)
 
-  response.status(204).end()
+response.status(204).end()
 })
 
 const generateId = () => {
