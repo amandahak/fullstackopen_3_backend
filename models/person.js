c9fedbdeb0368072f96eafd3465bb781db706bcc
Nadina <nadinalehtonen@kamk.fi>
2024-09-12 18:45:56 +0300

    numeron validointi korjattu

--- models/person.js
+++ models/person.js
@@ -16,24 +16,24 @@ mongoose.connect(url)
     console.log('error connecting to MongoDB:', error.message)
   })
 
-  const personSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      minlength: 3,
-      required: true
-    },
-    number: {
-      type: String,
-      minlength: 8,
-      validate: {
-        validator: function(v) {
-          return /^\d{2,3}-\d{5,}$/.test(v)
-        },
-        message: props => `${props.value} is not a valid phone number!`
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d{5,}$/.test(v)
       },
-      required: true
-    }
-  })
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: true
+  }
+})
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
